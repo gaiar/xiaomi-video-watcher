c7411cca5c6401d78f7938b5c09891c191c1dc81
gaiar <gaiar@baimuratov.ru>
2019-12-05 16:53:26 +0100

    change ffmpeg command

--- video-watcher/video_watchdog.py
+++ video-watcher/video_watchdog.py
@@ -88,6 +88,8 @@ class Handler(FileSystemEventHandler):
                     "-y",
                     "-filter_complex",
                     "[0:v] fps=12,scale=w=640:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1",
+                    "-flags",
+                    "+global_header",
                     tempfile,
                 ]
                 call(command)
