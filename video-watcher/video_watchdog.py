2b854003afc732c576c982d721947016ce7fbd51
gaiar <gaiar@baimuratov.ru>
2019-12-06 11:21:09 +0100

    Break into functions. Exceptions handling.

--- video-watcher/video_watchdog.py
+++ video-watcher/video_watchdog.py
@@ -16,6 +16,7 @@ from watchdog.observers import Observer
 from watchdog.observers.polling import PollingObserver
 import shutil
 
+
 def parse_config(config_path):
     with open(config_path, "r") as config_file:
         config_data = json.load(config_file)
@@ -62,105 +63,18 @@ class Handler(FileSystemEventHandler):
             filepath, tempfile, giffile, processfile = self._get_paths(event.src_path)
             if PurePath(filepath).suffix == ".mp4":
                 # TODO: Add timings
-                logging.info(
-                    "Processing file {0} with size of {1} MB".format(
-                        filepath.name, filepath.stat().st_size / 1024
-                    )
-                )
-                print(
-                    "Processing file {0} with size of {1} MB".format(
-                        filepath.name, filepath.stat().st_size / 1024
-                    )
-                )
-
-                # cmd = [ffmpeg,'-i', inFile,'-f', 'image2','-vf',
-                # "select='eq(pict_type,PICT_TYPE_I)'",'-vsync','vfr', imgFilenames]
-                command = [
-                    "MP4Box",
-                    "-inter",
-                    "500",
-                    tempfile
-                ]
-                call(command)
-# Running ffmpeg to gif conversion
-                command = [
-                    "ffmpeg",
-                    "-loglevel",
-                    "error",
-                    "-hide_banner",
-                    "-nostats",
-                    "-i",
-                    filepath,
-                    "-y",
-                    "-filter_complex",
-                    "[0:v] fps=12,scale=w=640:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1",
-                    "-flags",
-                    "+global_header",
-                    tempfile,
-                ]
-                call(command)
-
-                logging.info(
-                    "Created file {0} with size of {1} MB".format(
-                        tempfile.name, tempfile.stat().st_size / 1024
-                    )
-                )
-                print(
-                    "Created file {0} with size of {1} MB".format(
-                        tempfile.name, tempfile.stat().st_size / 1024
-                    )
-                )
-# Optimizing GIF file
-                command = [
-                    "gifsicle",
-                    "-f",
-                    "-O3",
-                    tempfile,
-                    "-o",
-                    giffile,
-                ]
-
-                call(command)
-                logging.info(
-                    "Created optimized file {0} with size of {1} MB".format(
-                        giffile.name, giffile.stat().st_size / 1024
-                    )
-                )
-                print(
-                    "Created optimized file {0} with size of {1} MB".format(
-                        giffile.name, giffile.stat().st_size / 1024
-                    )
-                )
-
-                #self._send_to_telegram(filepath)
-# Creating screenshot 
-
-#ffmpeg -ss 01:23:45 -i input -vframes 1 -q:v 2 output.jpg
-                command = [
-                    "ffmpeg",
-                    "-loglevel",
-                    "error",
-                    "-hide_banner",
-                    "-nostats",
-                    "-ss",
-                    "00:00:10",
-                    "-i",
-                    filepath,
-                    "-y",
-                    "-vframes",
-                    "1",
-                    "-q:v",
-                    "2",
-                    processfile.with_suffix(".jpg")]
-                call(command)
-                shutil.copy(str(filepath), str(processfile.with_suffix(".mp4")))
-
-                tempfile.unlink()
-                #filepath.unlink()
-
-                self._send_to_telegram(processfile.with_suffix(".mp4"))
-                logging.info("Deleted file {0}".format(tempfile.name))
-                print("Deleted file {0}".format(tempfile.name))
+                try:
+                    self._fix_moov(filepath)
+                    self._convert_gif(filepath, tempfile)
+                    self._optimize_gif(tempfile, giffile)
+                    self._create_screenshot(filepath,processfile)
+                    tempfile.unlink()
+                    logging.info("Deleted file {0}".format(tempfile.name))
+                    print("Deleted file {0}".format(tempfile.name))
+                except Exception as error:
+                    logging.error("Problem {0} converting file {1}".format(error, filepath))
+                self._send_to_telegram(filepath)
+               
 
             else:
                 logging.warning(
@@ -179,22 +93,110 @@ class Handler(FileSystemEventHandler):
         processfile = Path(self.config["video_processed_dir"], filename)
         return filepath, tempfile, giffile, processfile
 
+    def _fix_moov(self, videofile):
+        logging.info(
+            "Processing file {0} with size of {1} MB".format(
+                videofile.name, videofile.stat().st_size / 1024
+            )
+        )
+        print(
+            "Processing file {0} with size of {1} MB".format(
+                videofile.name, videofile.stat().st_size / 1024
+            )
+        )
+
+        command = ["MP4Box", "-inter", "500", videofile]
+        call(command)
+
+    def _convert_gif(self, filepath, tempfile):
+        command = [
+            "ffmpeg",
+            "-loglevel",
+            "error",
+            "-hide_banner",
+            "-nostats",
+            "-i",
+            filepath,
+            "-y",
+            "-filter_complex",
+            "[0:v] fps=12,scale=w=640:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1",
+            "-flags",
+            "+global_header",
+            tempfile,
+        ]
+        call(command)
+
+        logging.info(
+            "Created file {0} with size of {1} MB".format(
+                tempfile.name, tempfile.stat().st_size / 1024
+            )
+        )
+        print(
+            "Created file {0} with size of {1} MB".format(
+                tempfile.name, tempfile.stat().st_size / 1024
+            )
+        )
+
+    def _optimize_gif(self, tempfile, giffile):
+        command = [
+            "gifsicle",
+            "-f",
+            "-O3",
+            tempfile,
+            "-o",
+            giffile,
+        ]
+
+        call(command)
+        logging.info(
+            "Created optimized file {0} with size of {1} MB".format(
+                giffile.name, giffile.stat().st_size / 1024
+            )
+        )
+        print(
+            "Created optimized file {0} with size of {1} MB".format(
+                giffile.name, giffile.stat().st_size / 1024
+            )
+        )
+
+    def _create_screenshot(self, filepath, processfile):
+        command = [
+            "ffmpeg",
+            "-loglevel",
+            "error",
+            "-hide_banner",
+            "-nostats",
+            "-ss",
+            "00:00:10",
+            "-i",
+            filepath,
+            "-y",
+            "-vframes",
+            "1",
+            "-q:v",
+            "2",
+            processfile.with_suffix(".jpg"),
+        ]
+        call(command)
+        shutil.copy(str(filepath), str(processfile.with_suffix(".mp4")))
 
     def _send_to_telegram(self, filepath):
 
         for attempt in range(10):
             try:
                 logging.debug("Sending to telegram {0}".format(filepath))
-                self.updater.bot.send_video(
-                    chat_id="@baimuratov_bot_group", video=open(filepath, "rb"),
-                    duration=60,
-                    caption="Motion detected!",
-                    supports_streaming=True,
-                    disable_notification=True,
-                    timout=30
-                )
-                logging.debug("{0} sent successfully ".format(filepath))
-                break
+                with open(filepath, "rb") as videofile:
+                    self.updater.bot.send_video(
+                        chat_id="@baimuratov_bot_group",
+                        video=videofile,
+                        duration=60,
+                        caption="Motion detected!",
+                        supports_streaming=True,
+                        disable_notification=True,
+                        timout=30,
+                    )
+                    logging.debug("{0} sent successfully ".format(filepath))
+                    break
             except Exception as e:
                 print(e)
                 logging.warning(
@@ -203,6 +205,7 @@ class Handler(FileSystemEventHandler):
                 time.sleep(5)
 
         return None
+
 
 def main():
     logging.basicConfig(filename="video_watcher.log", level=logging.INFO)
@@ -214,7 +217,9 @@ def main():
 
     observer = PollingObserver()
     event_handler = Handler(config)
-    observer.schedule(event_handler, os.path.realpath(config["video_watch_dir"]), recursive=True)
+    observer.schedule(
+        event_handler, os.path.realpath(config["video_watch_dir"]), recursive=True
+    )
     observer.start()
 
     try:
